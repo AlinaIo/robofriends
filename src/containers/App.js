d323220f0eda249e2f39ec6a8ce575767723b2b6
alinaio <alinaionescu85@gmail.com>
2018-12-04 23:39:17 +0200

    added redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import '../containers/App.css';
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    console.log('=========', state);
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
             robots: [],
-            searchField: ''
         }
     }
 
@@ -21,12 +35,13 @@ class App extends Component {
     }
 
     //arrow function is used so that .this is referring to this in constructor
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value })
-    }
+    // onSearchChange = (event) => {
+    //     this.setState({ searchField: event.target.value })
+    // }
 
     render() {
-        const { robots, searchField } =  this.state;
+        const { robots } =  this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
@@ -35,15 +50,15 @@ class App extends Component {
        (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
-                        <ErrorBoundry>
+                        {/* <ErrorBoundry> */}
                             <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
+                        {/* </ErrorBoundry> */}
                     </Scroll>
                 </div>
             )        
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
